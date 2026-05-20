49fba4a52f94722728b15d63f4faa46078581b90
MeiLeng-Tan <meileng.tanml@gmail.com>
2026-05-20 03:37:17 -0400

    Task 2 and 3

---
+++ run_emotion_detector.py
@@ -0,0 +1,6 @@
+from emotion_detection import emotion_detector
+
+# result = emotion_detector("I love this new technology.")
+result = emotion_detector("I am so happy I am doing this.")
+
+print(result)
